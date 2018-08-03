e66761e123790c56d04a00f58fa6003d646b7596
C-Elliott44 <cameronjohnelliott@gmail.com>
2018-08-02 19:28:57 -0700

    tester OAuth log in with google on local host

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 const express = require('express');
 const authRoutes = require('./routes/auth-routes');
-const app = express();
+const profileRoutes = require('./routes/profile-routes');
 const passportSetup = require('./config/passport-setup');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const  passport = require('passport');
+
+const app = express();
 
 //set up view engine
 app.set('view engine', 'ejs');
 
+app.use(cookieSession({
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [keys.session.cookieKey]
+}));
+
+// initailize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+//connect to mongodb
+mongoose.connect(keys.mongodb.dbURI, () => {
+    console.log("connected to mongo DB");
+})
+
 // set up routes
 app.use('/auth', authRoutes);
+app.use('/profile', profileRoutes);
 
 //create home route
 app.get('/', (req, res) => {
